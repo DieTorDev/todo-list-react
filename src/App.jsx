fe1bc32a00fe17e6360ee406a459ff662445af86
ArisayMella <159270892+DieTorDev@users.noreply.github.com>
2024-06-18 12:54:18 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MainContainer from './components/MainContainer/MainContainer';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+const App = () => {
+	return (
+		<>
+			<GlobalStyles />
+			<MainContainer />
+		</>
+	);
+};
+
+export default App;
